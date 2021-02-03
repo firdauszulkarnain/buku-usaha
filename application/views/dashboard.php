9106cf44b7c2fefb17cf0f067535f61bf43beab7
firdauszulkarnain <muhammadfirdauszulkarnain03@gmail.com>
2021-02-03 16:48:06 +0800

    initial commit

---
+++ application/views/dashboard.php
@@ -0,0 +1,76 @@
+<div id="app">
+    <div class="main-wrapper">
+      <div class="navbar-bg"></div>
+      <div class="main-sidebar">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html"><i class="fas fa-book-open"></i> Buku Usaha</a>
+          </div>
+            <ul class="sidebar-menu">
+                <li class="menu-header">Dashboard</li>
+                <li><a class="nav-link" href="blank.html"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+                <li class="menu-header">Produk</li>
+                <li><a class="nav-link" href="blank.html"><i class="fas fa-list"></i> <span>Kategori Produk</span></a></li>
+                <li><a class="nav-link" href="blank.html"><i class="fas fa-box-open"></i> <span>Data Produk</span></a></li>
+                <li class="menu-header">Keuangan</li>
+                <li><a class="nav-link" href="blank.html"><i class="fas fa-arrow-alt-circle-left"></i><span>Pembelian</span></a></li>
+                <li><a class="nav-link" href="blank.html"><i class="fas fa-arrow-alt-circle-right"></i> <span>Penjualan</span></a></li>
+            </ul>
+        </aside>
+      </div>
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-icon shadow-primary bg-primary">
+                  <i class="fas fa-archive"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Penjualan</h4>
+                  </div>
+                  <div class="card-body">
+                    00
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-icon shadow-primary bg-primary">
+                  <i class="fas fa-dollar-sign"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Laba Bersih</h4>
+                  </div>
+                  <div class="card-body">
+                    Rp. 0
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-icon shadow-primary bg-primary">
+                  <i class="fas fa-shopping-bag"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Sales</h4>
+                  </div>
+                  <div class="card-body">
+                    4,732
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+        </section>
+      </div>
+
+
+
