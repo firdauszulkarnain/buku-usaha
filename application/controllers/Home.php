7727ae1b0a4e8b9913fe032e63311d369df51fa3
firdauszulkarnain <muhammadfirdauszulkarnain03@gmail.com>
2021-02-03 18:35:09 +0800

    tambah halaman kategori

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,11 +1,22 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller {
-    public function index(){
-       $this->load->view('dashboard');
+class Home extends CI_Controller
+{
+    public function index()
+    {
+        $data['title'] = 'Dashboard';
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar', $data);
+        $this->load->view('dashboard');
+        $this->load->view('template/footer');
     }
-public function about(){
-    
-}
+    public function category()
+    {
+        $data['title'] = 'Kategori Produk';
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar', $data);
+        $this->load->view('category');
+        $this->load->view('template/footer');
+    }
 }
---
+++ application/views/template/sidebar.php
@@ -0,0 +1,51 @@
+<div class="navbar-bg"></div>
+<nav class="navbar navbar-expand-lg main-navbar">
+    <ul class="navbar-nav mr-3">
+        <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a>
+        <h6 class="mt-2 text-light"><?= $title; ?></h6>
+    </ul>
+</nav>
+</div>
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html"><i class="fas fa-book-open"></i> Buku Usaha</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li>
+                <a class="nav-link" href="#">
+                    <i class="fas fa-fire"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="menu-header">Produk</li>
+            <li>
+                <a class="nav-link" href="<?= base_url(); ?>/home/category">
+                    <i class="fas fa-list"></i>
+                    <span>Kategori Produk</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="#">
+                    <i class="fas fa-box-open"></i>
+                    <span>Data Produk</span>
+                </a>
+            </li>
+            <li class="menu-header">Keuangan</li>
+            <li>
+                <a class="nav-link" href="#">
+                    <i class="fas fa-arrow-alt-circle-left">
+                    </i>
+                    <span>Pembelian</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="#">
+                    <i class="fas fa-arrow-alt-circle-right"></i>
+                    <span>Penjualan</span>
+                </a>
+            </li>
+        </ul>
+    </aside>
+</div>
