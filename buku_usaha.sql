abfe6d774ad9c1bbfda95d6d6339385f8322c7aa
firdauszulkarnain <muhammadfirdauszulkarnain03@gmail.com>
2021-09-01 18:12:33 +0800

    add new database

--- buku_usaha.sql
+++ buku_usaha.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2021 at 11:54 AM
+-- Generation Time: Sep 01, 2021 at 12:10 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.3.16
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `kategori` (
   `id_kategori` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `nama_kategori` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,7 +41,8 @@ CREATE TABLE `kategori` (
 
 CREATE TABLE `pembelian` (
   `id_pembelian` int(11) NOT NULL,
-  `produk_id` int(11) NOT NULL,
+  `produk_name` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `unit` int(11) NOT NULL,
   `total_beli` decimal(10,0) NOT NULL,
   `tanggal_beli` date NOT NULL
@@ -54,7 +56,8 @@ CREATE TABLE `pembelian` (
 
 CREATE TABLE `penjualan` (
   `id_penjualan` int(11) NOT NULL,
-  `produk_id` int(11) NOT NULL,
+  `produk_name` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `unit` int(11) NOT NULL,
   `total_untung` int(11) NOT NULL,
   `tanggal_jual` date NOT NULL
@@ -66,7 +69,7 @@ CREATE TABLE `penjualan` (
 DELIMITER $$
 CREATE TRIGGER `Update_Stok` AFTER INSERT ON `penjualan` FOR EACH ROW BEGIN
 UPDATE produk SET stock = stock-NEW.unit
-WHERE id_produk = NEW.produk_id;
+WHERE nama_produk = NEW.produk_name;
 END
 $$
 DELIMITER ;
@@ -83,8 +86,29 @@ CREATE TABLE `produk` (
   `hrg_beli` decimal(10,0) NOT NULL,
   `hrg_jual` decimal(10,0) NOT NULL,
   `stock` int(11) NOT NULL,
-  `kategori_id` int(11) NOT NULL
+  `kategori_name` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_username` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `namaUsaha` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_username`, `username`, `namaUsaha`, `password`) VALUES
+(23, 'firdaus', 'Firdaus Speaker', '$2y$10$HuBsAuA/bYgJ4zE2SfZTR.I1E8jOeAGKLUUU1qaCxRfc2sNBvbJK2');
 
 --
 -- Indexes for dumped tables
@@ -115,6 +139,12 @@ ALTER TABLE `produk`
   ADD PRIMARY KEY (`id_produk`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -122,25 +152,31 @@ ALTER TABLE `produk`
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `pembelian`
 --
 ALTER TABLE `pembelian`
-  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `penjualan`
 --
 ALTER TABLE `penjualan`
-  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_username` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
